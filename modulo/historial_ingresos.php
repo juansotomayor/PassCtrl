ce99b5b624991da4b759d686f5f9f4df01b2cadc
Juan Sotomayor <juan.sotomayor.ch@gmail.com>
2017-01-06 00:51:28 -0300

    Agregar filtros fechas

---
+++ modulo/historial_ingresos.php
@@ -0,0 +1,243 @@
+<?php			
+  error_reporting(E_ERROR | E_WARNING | E_PARSE);
+  session_start();
+  if( isset($_SESSION['ingreso']) || isset($_COOKIE['nueva'])){
+    if(!isset($_SESSION['ingreso']) && isset($_COOKIE['nueva'])){
+      $_SESSION['ingreso'] = $_COOKIE['nueva'];		
+    }
+    $var = 0;
+  }else{
+    $URL="/passctrl/";
+    echo "<script>location.href='$URL'</script>";
+    $var = 1;
+  }
+  $modulo = 1;
+?>
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+  <link rel="stylesheet" href="/passctrl/css/tables.css" type="text/css" />
+	<link rel="stylesheet" href="/passctrl/css/input.css" type="text/css" />
+  <link rel="stylesheet" href="/passctrl/css/style.css" type="text/css" />
+  <link rel="stylesheet" href="/comercializadora/css/font-awesome.min.css">
+  <link rel="stylesheet" href="/passctrl/css/drop.css" type="text/css" />
+  <link rel="stylesheet" href="/passctrl/css/estadisticas.css" type="text/css" />
+
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+  <script src="http://code.jquery.com/jquery-latest.js"></script>
+	<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js'></script>
+  <script src="/passctrl/js/modernizr.js"></script>
+  <script src="/passctrl/js/inputfile.js"></script>
+  <script src="/passctrl/js/efectos.js"></script>
+  <script src="/passctrl/js/tooltip.js"></script>
+  <script type="text/javascript" src="/comercializadora/js/jquery.Rut.min.js"></script>
+  <script type="text/javascript" src="/comercializadora/js/jquery.Rut.js"></script>
+  <link rel="stylesheet" href="/passctrl/js/tooltip.css">
+  <script>
+  
+  /********************************/
+  function historial(modulo)
+	{
+    
+    var fecha1 = document.getElementById('fecha_inicio').value;
+    var fecha2 = document.getElementById('fecha_termino').value;
+    
+     	
+    //alert(R_Shipper+"-"+R_Embarque+"-"+R_Carton+"-"+R_Palet+"-"+R_SO+"-"+R_Detalle+"-"+R_Imagen+"-"+R_Modelo+"-"+R_Linea+"-"+R_Qty+"-"+R_Prodid+"-"+R_Para+"/");
+    if(fecha1){
+      var fd = new FormData();	
+      
+      fd.append("fecha1", fecha1);
+      fd.append("fecha2", fecha2);
+      fd.append("modulo", modulo);
+      fd.append("buscar", 'historial');
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "usuarios_estadisticas.php");
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+          var r = xhr.responseText;	
+          document.getElementById("historial").innerHTML = r;
+          
+        }
+      };
+      xhr.send(fd);
+    }
+    
+	}
+  function fecha2()
+	{
+    var fecha1 = document.getElementById('fecha_inicio').value;
+    var fecha2 = document.getElementById('fecha_termino').value;
+    if(!fecha2){
+      document.getElementById('fecha_termino').value=fecha1;
+    }
+    
+	}
+  </script>
+</head>
+
+<body class="">
+  <div class="container">
+    <div class="breadcrumbs">
+      <img style="top: 7px; left: 80px; height: 45px; z-index: 2; position: absolute;" src="/passctrl/img/passctrl.png">      
+      <ul class="social">
+        <?php
+            include("sesion.php");
+        ?>
+      </ul>
+    </div>
+    <header class="clearfix">
+      <h1>Historial</h1> 
+    </header>
+    <!--End Header -->
+    <ul class="tl-menu">
+      <li><a href="#">Logo</a></li>
+      <li class="tl-current"><a title="Ver modulos" href="/passctrl/modulo/" class="entypo-shareable" id="navItem1">Option 1</a></li>
+       <!--
+      <li><a href="#" class="icon-chart" id="navItem2">Option 2</a></li>
+      <li><a href="#" id="navItem3">Option 3</a></li>
+      <li> <a href="#" class="icon-download" id="navItem4">Active</a></li>
+      <li><a href="#" class="entypo-network" id="navItem5">Option 4</a></li>
+      <li><a href="#" class="icon-lamp" id="navItem6">Option 5</a></li>
+      <li><a href="#" class="icon-file" id="navItem7">Option 6</a></li>
+      -->
+    </ul>
+    
+    <div id="main_usuarios" class="main">  
+        <?php 
+          $modulo = 1;
+          $sql2 = "SELECT * FROM ingreso WHERE idModulo = '$modulo'AND fecha=(SELECT MIN(fecha) FROM ingreso WHERE idModulo = '$modulo')";
+          $query2 = mysqli_query($conn,$sql2);
+          $numero2 =  $query2->num_rows;
+          if($numero2 != 0 ){
+              while ($registro = mysqli_fetch_assoc($query2)){  
+                  $min=date_create($registro['fecha']);  
+                  $min =date_format($min, 'Y-m-d');    
+              }
+          }
+
+          $sql2 = "SELECT * FROM ingreso WHERE idModulo = '$modulo' AND fecha=(SELECT MAX(fecha) FROM ingreso WHERE idModulo = '$modulo')";
+          $query2 = mysqli_query($conn,$sql2);
+          $numero2 =  $query2->num_rows;
+          if($numero2 != 0 ){
+              while ($registro = mysqli_fetch_assoc($query2)){ 
+                  $max=date_create($registro['fecha']);  
+                  $max= date_format($max, 'Y-m-d'); 
+              }
+          }
+          echo ' 
+          <div class="buscador de fechas">
+            <table>
+              <tr>
+                <td>
+                  Fecha de inicio
+                </td>
+                <td>
+                  <input type="date" id="fecha_inicio" onchange="fecha2()" min="'.$min.'" max="'.$max.'">
+                </td>
+                <td>
+                  Fecha de inicio
+                </td>
+                <td>
+                  <input type="date" id="fecha_termino"  min="'.$min.'" max="'.$max.'">
+                </td>
+                <td>
+                  <button onclick="historial('.$modulo.')" class="btn blue" type="button">Filtrar</button>
+                </td>
+              </tr>
+            </table>
+          </div>
+          ';
+        ?> 
+        <div id="historial">
+
+        </div>           
+    </div>
+    <!--End Main -->
+
+    <!--Slider Nav 1-->
+
+    <nav class="slider-menu slider-menu-vertical slider-menu-left" id="slider-menu-s1">
+      <h3>MODULO</h3>
+      <a href="/passctrl/modulo/usuarios.php" ><i class="fa fa-users" aria-hidden="true"></i>&nbsp;<p>Usuarios</p></a>
+      <a href="/passctrl/modulo/agregar_usuario.php"><i  class="fa fa-user-plus" aria-hidden="true"></i>&nbsp;<p>Agregar Usuario</p></a>
+      <a href="/passctrl/modulo/historial_ingresos.php"><i  class="fa fa-calendar-check-o" aria-hidden="true"></i>&nbsp;<p>Historial</p></a>      
+      <!--<a href="#">Item 3</a>
+      <a href="#">Item 4</a>
+      <a href="#">Item 5</a>
+      <a href="#">Item 6</a>
+      <a href="#">Item 7</a>
+      -->
+    </nav>
+
+  </div>
+  <!--End Containter -->
+
+
+  <!-- Classie - class helper functions by @desandro https://github.com/desandro/classie -->
+
+  <!-- Add id=navItem# to nav and add a seperate function below to match the id. Each nav item must have a unique id navItem1#. -->
+  <script src="js/classie.js"></script>
+  <script>
+    var menuLeft = document.getElementById( 'slider-menu-s1' ),
+    				showLeft = document.getElementById( 'showLeft' ),
+    				body = document.body;
+    
+    			navItem1.onclick = function() {
+    				classie.toggle( this, 'active' );
+    				classie.toggle( menuLeft, 'slider-menu-open' );
+    				disableOther( 'navItem1' );
+    			};
+          
+    			navItem2.onclick = function() {
+    				classie.toggle( this, 'active' );
+    				classie.toggle( menuLeft, 'slider-menu-open' );
+    				disableOther( 'navItem2' );
+    			};
+          
+          navItem3.onclick = function() {
+    				classie.toggle( this, 'active' );
+    				classie.toggle( menuLeft, 'slider-menu-open' );
+    				disableOther( 'navItem3' );
+    			};
+          
+          navItem4.onclick = function() {
+    				classie.toggle( this, 'active' );
+    				classie.toggle( menuLeft, 'slider-menu-open' );
+    				disableOther( 'navItem4' );
+    			};
+          
+    			navItem5.onclick = function() {
+    				classie.toggle( this, 'active' );
+    				classie.toggle( menuLeft, 'slider-menu-open' );
+    				disableOther( 'navItem5' );
+    			};
+          
+          navItem6.onclick = function() {
+    				classie.toggle( this, 'active' );
+    				classie.toggle( menuLeft, 'slider-menu-open' );
+    				disableOther( 'navItem6' );
+    			};
+          
+          navItem7.onclick = function() {
+    				classie.toggle( this, 'active' );
+    				classie.toggle( menuLeft, 'slider-menu-open' );
+    				disableOther( 'navItem7' );
+    			};
+    
+    			function disableOther( button ) {
+    				if( button !== 'showLeft' ) {
+    					classie.toggle( showLeft, 'disabled' );
+    				}
+    			}
+  </script>
+
+
+</body>
+
+</html>
--- modulo/usuarios_estadisticas.php
+++ modulo/usuarios_estadisticas.php
@@ -7,6 +7,8 @@ include("../connectmysql.php");
 $modulo = mysqli_real_escape_string($conn,$_POST['modulo']);
 $usuario = mysqli_real_escape_string($conn,$_POST['usuario']);
 $fecha = mysqli_real_escape_string($conn,$_POST['fecha']);
+$fecha_his = mysqli_real_escape_string($conn,$_POST['fecha1']);
+$fecha_his2 = mysqli_real_escape_string($conn,$_POST['fecha2']);
 $buscar = mysqli_real_escape_string($conn,$_POST['buscar']);
 if($buscar == "usuario"){
     $sql2 = "SELECT * FROM ingreso WHERE idModulo = '$modulo' AND id='$usuario' AND fecha=(SELECT MIN(fecha) FROM ingreso WHERE idModulo = '$modulo' AND id='$usuario')";
@@ -82,6 +84,97 @@ if($buscar == "usuario"){
         </table>
         ';
     }
+}else if($buscar == "historial"){
+    
+    $fecha_his = date_create($fecha_his);
+    $fecha_his= date_format($fecha_his, 'Y-m-d'); 
+    $fecha_his2 = date_create($fecha_his2);
+    $fecha_his2= date_format($fecha_his2, 'Y-m-d');
+    //$sql2 = "SELECT * FROM ingreso WHERE fecha >= '$fecha_his' ";
+    $sql2 = "SELECT * FROM ingreso WHERE DATE(fecha) BETWEEN '$fecha_his' AND '$fecha_his2' "; //ORDER BY fecha DESC
+    $query2 = mysqli_query($conn,$sql2);
+    $numero2 =  $query2->num_rows;
+    if($numero2 != 0 ){
+        echo'
+        <h3>Reporte correspondiente a la fecha '.$fecha_his.'</h3>
+        <table class="list_usuarios">
+            <thead>
+                <tr class="tr">
+                  <th class="th" colspan=2>Usuario</th>
+                  <th class="th">Empresa</th>
+                  <th class="th">Acceso</th>                  
+                  <th class="th">Tipo</th>	
+                  <th class="th">Fecha</th>	
+                </tr>
+            </thead>
+            <tbody>
+        ';
+        $Q =0;
+        while ($registro = mysqli_fetch_assoc($query2)){ 
+            $Q++; 
+            $id[$Q] = $registro['id'];
+            if($registro['estado'] == '1'){
+                $estado[$Q] = '<i class="fa fa-check verde fa-2x" aria-hidden="true"></i>';
+            }else {         
+                $estado[$Q] = '<i class="fa fa-times rojo fa-2x" aria-hidden="true"></i>';
+           }
+            if($registro['tipo'] == 'BIOMETRIA'){
+                $tipo[$Q] = '<img title="Biometria" class="manImg" src="/passctrl/img/icon/Fingerprint Scan-50.png"></img>';
+            }else if($registro['tipo'] == 'RFID'){
+                $tipo[$Q] = '<img title="RFID" class="manImg" src="/passctrl/img/icon/RFID Tag Filled-50.png"></img>';
+           }else{
+                $tipo = '-';
+            }
+            $fecha_reg[$Q] = $registro['fecha'];    
+         } 
+    }
+    if($Q != 0){
+        $modulo = '1';
+        for($i=1; $i<=$Q; $i++){
+            $sql2 = "SELECT * FROM usuarios_modulos WHERE id = '$id[$i]'";
+            $query2 = mysqli_query($conn,$sql2);
+            $numero2 =  $query2->num_rows;
+            if($numero2 != 0 ){
+                while ($registro = mysqli_fetch_assoc($query2)){ 
+                    $nombre[$i]=$registro['nombre'];
+                    $apellido[$i]=$registro['apellido'];
+                    $imagen[$i]=$registro['imagen'];
+                    $empresa[$i]=$registro['empresa'];
+                    echo'
+                    <tr>
+                        <td class="td" class="tdfoto">
+                            <div id="img_perfil">';
+                            if($imagen[$i]){
+                                echo'
+                                <img src="/passctrl/modulo/img/usuarios/'.$imagen[$i].'">
+                                ';
+                            }else{
+                                echo'
+                                <img src="/passctrl/img/usuarios/anonimo.jpg">
+                                ';
+                            }
+                            echo '
+                            </div>
+                        </td>
+                        <td class="td">'.$nombre[$i]." ".$apellido[$i].'</td>
+                        <td class="td">'.$empresa[$i].'</td>
+                        <td class="td">'.$estado[$i].'</td>
+                        <td class="td">'.$tipo[$i].'</td>
+                        <td class="td">'.$fecha_reg[$i].'</td>
+                    </tr>
+                    ';
+                }
+            }
+
+        }
+        echo '
+            </tbody>
+        </table>
+        ';      
+    }
+    
 }
+
+
 
 ?>
